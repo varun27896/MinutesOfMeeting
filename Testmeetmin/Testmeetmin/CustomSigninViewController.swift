8460a60c88955a4ef8fdec1b900526757ee2bf54
varun27896 <varun27896@gmail.com>
2019-10-31 14:15:28 -0500

    Signin and signup UI changed

---
+++ Testmeetmin/Testmeetmin/CustomSigninViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CustomSigninViewController.swift
+//  Testmeetmin
+//
+//  Created by Varun Chillara on 10/22/19.
+//  Copyright © 2019 Varun Chillara. All rights reserved.
+//
+
+import UIKit
+import AWSMobileClient
+
+class CustomSigninViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInBtnTapped(_ sender: Any) {
+        self.signIn()
+    }
+    
+    func signIn(){
+        AWSMobileClient.default().signIn(username: usernameTextField.text!, password: passwordTextField.text!) { (signInResult, error) in
+            if let error = error  {
+                print("\(error.localizedDescription)")
+            } else if let signInResult = signInResult {
+                switch (signInResult.signInState) {
+                case .signedIn:
+                    print("User is signed in.")
+                case .smsMFA:
+                    print("SMS message sent to \(signInResult.codeDetails!.destination!)")
+                default:
+                    print("Sign In needs info which is not et supported.")
+                }
+            }
+        }
+    }
+
+}
